a2e612ad0ab379d719ecb937527b135cfcbc946e
Colum Kelly <columk1@users.noreply.github.com>
2023-10-14 23:15:23 -0700

    Create data model

---
+++ src/assets/data-model.js
@@ -0,0 +1,48 @@
+export default {
+  person: {
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    phone: '+1 123 456 7890',
+    location: 'Florida, USA',
+  },
+  experience: [
+    {
+      jobTitle: 'Senior Software Engineer',
+      employer: 'Google',
+      location: 'New York, USA',
+      startDate: 'Oct, 2022',
+      endDate: 'present',
+      description: [
+        'In the crypto space, industry is globally virtualising its next-generation team players.',
+        'Our smart contract development lifecycle enables long-term, best-in-class products.',
+        'Relayed dot-bombs to intelligently and effectively monetize our unparalleled bandwidth.',
+      ],
+    },
+    {
+      jobTitle: 'Meta Development Developer Engineer',
+      employer: 'Microsoft',
+      location: 'Seattle, USA',
+      startDate: 'Jan, 2021',
+      endDate: 'Feb, 2022',
+      description: [
+        'Used holistic user experiences to globally manage our executive search expectations.',
+        'Unparalleled big datas ethically enable company-wide prince2 practitioners for our synergies.',
+        'Efficiencies will come from conservatively calibrating our intrapreneurs.',
+      ],
+    },
+  ],
+  education: [
+    {
+      school: 'University of California, Berkeley',
+      degree: 'Bachelor of Science',
+      startDate: 'Oct, 2020',
+      endDate: 'May, 2022',
+    },
+  ],
+  skills: {
+    languages: 'JavaScript, Python, TypeScript, Go',
+    technologies: 'MySQL, AWS, Git, React, Node, Docker',
+    other: 'Data Structures and Algorithms, Testing, UX Design',
+  },
+  links: ['https://github.com/johndoe', 'https://linkedin.com/in/johndoe'],
+}
