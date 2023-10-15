a28842147e51be37760b5455bb8d2712234ee962
Colum Kelly <columk1@users.noreply.github.com>
2023-10-15 13:01:01 -0700

    Fix: cv padding

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -15,42 +15,44 @@ export default function CV({ data }) {
           <p>{person.email}</p>
         </div>
       </div>
-      <div className='section experience'>
-        <h3 className='cv-section-title'>Experience</h3>
-        <div className='h-divider'></div>
-        <div className='timeline'>
-          {experience.map((job, index) => (
-            <div className='job' key={index}>
-              <div className='info-group'>
-                <h4>{job.jobTitle}</h4>
-                <p className='city'>{job.location}</p>
-              </div>
+      <div className='cv-body'>
+        <div className='section experience'>
+          <h3 className='cv-section-title'>Experience</h3>
+          <div className='h-divider'></div>
+          <div className='timeline'>
+            {experience.map((job, index) => (
+              <div className='job' key={index}>
+                <div className='info-group'>
+                  <h4>{job.jobTitle}</h4>
+                  <p className='city'>{job.location}</p>
+                </div>
 
-              <p className='employer'>{job.employer}</p>
+                <p className='employer'>{job.employer}</p>
+                <p className='dates'>
+                  {job.startDate} - {job.endDate}
+                </p>
+                <ul className='description'>
+                  {job.description.map((li, i) => (
+                    <li key={i}>{li}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className='section education'>
+          <h3 className='cv-section-title'>Education</h3>
+          <div className='h-divider'></div>
+          {education.map((school, index) => (
+            <div className='school' key={index}>
+              <h4>{school.school}</h4>
+              <p className='degree'>{school.degree}</p>
               <p className='dates'>
-                {job.startDate} - {job.endDate}
+                {school.startDate} - {school.endDate}
               </p>
-              <ul className='description'>
-                {job.description.map((li, i) => (
-                  <li key={i}>{li}</li>
-                ))}
-              </ul>
             </div>
           ))}
         </div>
-      </div>
-      <div className='section education'>
-        <h3 className='cv-section-title'>Education</h3>
-        <div className='h-divider'></div>
-        {education.map((school, index) => (
-          <div className='school' key={index}>
-            <h4>{school.school}</h4>
-            <p className='degree'>{school.degree}</p>
-            <p className='dates'>
-              {school.startDate} - {school.endDate}
-            </p>
-          </div>
-        ))}
       </div>
     </div>
   )
