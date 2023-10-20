5e92419e7b16fe46e406c615d25cce4c2b4137b8
Colum Kelly <columk1@users.noreply.github.com>
2023-10-20 14:10:21 -0700

    Add global css variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import './styles/variables.css'
 import { Suspense } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './general/Router.jsx'
 import { Loading } from './components/Loading.jsx'
 
 export const PageWithHeader = ({ children }) => (
-  <div className="main-wrapper">{children}</div>
+  <div className='main-wrapper'>{children}</div>
 )
 
 export const App = () => (
@@ -12,7 +13,7 @@ export const App = () => (
     <Suspense
       fallback={
         <PageWithHeader>
-          <Loading name="suspense" />
+          <Loading name='suspense' />
         </PageWithHeader>
       }
     >
