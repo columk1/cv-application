9e07cf149494e45761ae97c1666736123f21249d
Colum Kelly <columk1@users.noreply.github.com>
2023-10-21 13:57:26 -0700

    Style add section button

--- src/components/AddSectionButton.jsx
+++ src/components/AddSectionButton.jsx
@@ -2,7 +2,7 @@ import '../styles/AddSectionButton.css'
 
 export default function AddSectionButton({ onClick, sectionTitle }) {
   return (
-    <button className='add-section-button' onClick={onClick}>
+    <button className='add-section-btn' onClick={onClick}>
       <span>+</span> Add {sectionTitle}
     </button>
   )
