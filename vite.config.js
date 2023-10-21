3d2e8c9f4dc23c7f7d45aa8b2d8e288c43ade83f
Colum Kelly <columk1@users.noreply.github.com>
2023-10-20 21:21:54 -0700

    Edit styles

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     webfontDownload([
-      'https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,400;9..40,500&family=DM+Serif+Display&family=Oswald:wght@300&display=swap&family=Material+Symbols+Outlined',
+      'https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,400;9..40,500&family=Roboto:wght@500&family=DM+Serif+Display&family=Oswald:wght@300&display=swap&family=Material+Symbols+Outlined',
     ]),
   ],
   test: {
